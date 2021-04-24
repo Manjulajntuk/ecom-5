8c4bb6343941acc391c428aab62e96008176fd99
Manjula <kamarajugaddamanjula@gmail.com>
2021-04-24 19:19:31 +0530

    Add files via upload

---
+++ frontend/src/data.js
@@ -0,0 +1,60 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default  {
+    products: [
+        {
+            _id:'1',
+        name:'zero slim shirt',
+        category: 'shirt',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+
+    },
+    {
+        _id:'2',
+        name:'one slim pants',
+        category: 'pants',
+        image: '/images/d1.jpg',
+        price: 640,
+        brand: 'Nike',
+        rating: 4.1,
+        numReviews: 10
+
+    },
+    {
+        _id:'3',
+        name:'two slim shirt',
+        category: 'shirt',
+        image: '/images/d1.jpg',
+        price: 650,
+        brand: 'Nike',
+        rating: 5,
+        numReviews: 10
+
+    },
+    {
+        _id:'4',
+        name:'three slim shirt',
+        category: 'shirt',
+        image: '/images/d1.jpg',
+        price: 690,
+        brand: 'Nike',
+        rating: 4,
+        numReviews: 10
+
+    },
+    {
+        _id:'5',
+        name:'four slim shirt',
+        category: 'pants',
+        image: '/images/d1.jpg',
+        price: 160,
+        brand: 'Nike',
+        rating: 4.3,
+        numReviews: 10
+
+    },
+]
+}
